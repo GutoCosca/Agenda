8ad646420ca9abe06c478c459ec06665b3a20627
Gustavo Coscarello <102304647+GutoCosca@users.noreply.github.com>
2023-03-29 16:27:27 -0300

    atualizando PHP

--- dados.php
+++ dados.php
@@ -7,9 +7,22 @@
     <title>Dados</title>
 </head>
 <body>
-    <?php 
-        $nome ="Gustavo";
-        echo 'Gustavo';
+    <?php
+        $nome = $_GET ["nome"];
+        $sobrenome = $_GET ["sobrenome"];
+        $natal = $_GET ["natal"];
+        $tel01 = $_GET ['tel01'];
+        $tel02 = $_GET ["tel02"];
+        $cel = $_GET ["cel"];
+        $email = $_GET ["email"];
+        $endResid = $_GET ["endResid"];
+        $numero = $_GET ["numero"];
+        $complemento = $_GET ["complemento"];
+        $bairro = $_GET ["bairro"];
+        $cep = $_GET ["cep"];
+        $cidade = $_GET ["cidade"];
+        $uf = $_GET ["uf"];
+        echo "Meu nome é $nome $sobrenome, nasci em $natal tel01 $tel01, tel02 $tel02, Celular $cel, e-mail $email end $endResid, $numero - $complemento, Bairro: $bairro - CEP: $cep - Cidade: $cidade - UF: $uf";
     ?>
 </body>
 </html>
