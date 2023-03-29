28c9c327cc70a4985cb2ce83db51a113b1807660
Gustavo Coscarello <102304647+GutoCosca@users.noreply.github.com>
2023-03-29 17:33:14 -0300

    atulização media query

--- dados.php
+++ dados.php
@@ -40,7 +40,6 @@
         $cidadeCom = $_GET ["cidadeCom"];
         $ufCom = $_GET ["ufCom"];
 
-        echo "Meu nome é $nome $sobrenome, nasci em $natal tel01 $tel01, tel02 $tel02, Celular $cel, e-mail $email end $endResid, $numero - $complemento, Bairro: $bairro - CEP: $cep - Cidade: $cidade - UF: $uf Empresa: $empresa";
     ?>
 </body>
 </html>
